d3adb348ea0caa74f385bb16b1c399b1be6b0108
Leo (Liutong) Zhou <liutong.zhou@apple.com>
2024-11-09 09:07:29 -0500

    Add the LLaMAMLP layer

--- src/gptx/layers/mlp.py
+++ src/gptx/layers/mlp.py
@@ -1,6 +1,7 @@
 import torch
-import torch.nn as nn
-from litgpt.config import Config
+from torch import nn
+from torch.nn import functional as F
+from .._base import Config
 
 
 class LLaMAMLP(nn.Module):
@@ -8,12 +9,13 @@ class LLaMAMLP(nn.Module):
         super().__init__()
         self.fc_1 = nn.Linear(config.n_embd, config.intermediate_size, bias=config.bias)
         self.fc_2 = nn.Linear(config.n_embd, config.intermediate_size, bias=config.bias)
-        self.proj = nn.Linear(config.intermediate_size, config.n_embd, bias=config.bias)
-
+        self.proj_o = nn.Linear(
+            config.intermediate_size, config.n_embd, bias=config.bias
+        )
         self.config = config
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x_fc_1 = self.fc_1(x)
         x_fc_2 = self.fc_2(x)
-        x = torch.nn.functional.silu(x_fc_1) * x_fc_2
-        return self.proj(x)
+        x = F.silu(x_fc_1) * x_fc_2
+        return self.proj_o(x)
